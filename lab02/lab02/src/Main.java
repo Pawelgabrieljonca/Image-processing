54f5379d9e42ce1d8ecbef788ab136fc4d194ac8
Pawelgabrieljonca <paweljonca30@gmail.com>
2024-10-19 23:35:06 +0200

    Small changes

--- lab02/lab02/src/Main.java
+++ lab02/lab02/src/Main.java
@@ -8,29 +8,14 @@ public class Main {
         Mat image = Imgcodecs.imread("dog.jpg");  // Wczytanie "dog.jpg" do obiektu typu Mat (macierzy)
 
         if (image.empty()) {
-            System.out.println("Mission failed");
+            System.out.println("Nie udało się wczytać obrazu, spróbuj ponownie");
             return;
         }
+        // Powiększanie obrazu o współczynnik 1,5
+        Mat resizedImage = new Mat();
+        Imgproc.resize(image, resizedImage, new Size(image.cols() * 1.5, image.rows() * 1.5), 0, 0, Imgproc.INTER_LINEAR);
 
-        // Pomniejszanie obrazu za pomocą resize
-        Mat resizedHalf = new Mat();
-        Mat resizedQuarter = new Mat();
-        Imgproc.resize(image, resizedHalf, new Size(image.cols() / 2, image.rows() / 2), 0, 0, Imgproc.INTER_LINEAR);
-        Imgproc.resize(image, resizedQuarter, new Size(image.cols() / 4, image.rows() / 4), 0, 0, Imgproc.INTER_LINEAR);
-
-        // Zapisywanie pomniejszonych obrazów za pomocą resize
-        Imgcodecs.imwrite("resized_half.jpg", resizedHalf);
-        Imgcodecs.imwrite("resized_quarter.jpg", resizedQuarter);
-
-        // Pomniejszanie obrazu za pomocą pyrDown
-        Mat pyrDownHalf = new Mat();
-        Mat pyrDownQuarter = new Mat();
-        Imgproc.pyrDown(image, pyrDownHalf, new Size(image.cols() / 2, image.rows() / 2));
-        Imgproc.pyrDown(pyrDownHalf, pyrDownQuarter, new Size(image.cols() / 4, image.rows() / 4));
-
-        // Zapisywanie pomniejszonych obrazów za pomocą pyrDown
-        Imgcodecs.imwrite("pyrdown2.jpg", pyrDownHalf);
-        Imgcodecs.imwrite("pyrdown4.jpg", pyrDownQuarter);
-
+        // Zapisywanie powiększonego obrazu
+        Imgcodecs.imwrite("resized_1_5x.jpg", resizedImage);
     }
 }
