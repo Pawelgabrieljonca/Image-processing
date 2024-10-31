732d7168496e046e022813525fd64d8e310d851e
Pawelgabrieljonca <paweljonca30@gmail.com>
2024-10-31 09:34:14 +0100

    some

--- lab03/somecode/src/Main.java
+++ lab03/somecode/src/Main.java
@@ -3,45 +3,45 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 public class Main {
     static { System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+
     public static void main(String[] args) {
-        Mat img = Imgcodecs.imread("women.png", Imgcodecs.IMREAD_GRAYSCALE);
+        Mat img = Imgcodecs.imread("p.png", Imgcodecs.IMREAD_GRAYSCALE);
+
         if (img.empty()) {
             System.out.println("Failed to load image.");
             return;
         }
-        // Segmentacja poprzez binaryzację
+        // Binaryzacja obrazu
         Mat binaryImg = new Mat();
         Imgproc.threshold(img, binaryImg, 127, 255, Imgproc.THRESH_BINARY);
 
-        // Definicje rozmiaru i kształtu elementu strukturalnego
-        Size kernelSize = new Size(3, 3);
-        int shape = Imgproc.MORPH_RECT;
-        Mat element = Imgproc.getStructuringElement(shape, kernelSize);
+        // Szkieletowanie przy pomocy iteracyjnej erozji i dylatacji
+        Mat skeleton = Mat.zeros(binaryImg.size(), CvType.CV_8UC1);
+        Mat temp = new Mat();
+        Mat eroded = new Mat();
+        Mat element = Imgproc.getStructuringElement(Imgproc.MORPH_CROSS, new Size(3, 3));
+        boolean done;
+        do {
+            // Wykonanie erozji
+            Imgproc.erode(binaryImg, eroded, element);
 
-        // Dylatacja obrazu
-        Mat dilatedImg = new Mat();
-        Imgproc.dilate(binaryImg, dilatedImg, element);
+            // Wykonanie dylatacji na erodowanym obrazie
+            Imgproc.dilate(eroded, temp, element);
 
-        // Erozja obrazu
-        Mat erodedImg = new Mat();
-        Imgproc.erode(binaryImg, erodedImg, element);
+            // Odejmowanie, aby uzyskać różnicę
+            Core.subtract(binaryImg, temp, temp);
 
-        // Metoda 1: Kontury przez dylatację
-        Mat contoursByDilation = new Mat();
-        Core.subtract(dilatedImg, binaryImg, contoursByDilation);
-        Imgcodecs.imwrite("contours_by_dilation.png", contoursByDilation);
-        System.out.println("Zapisano: contours_by_dilation.png");
+            // Sumowanie wyniku z dotychczasowym szkieletem
+            Core.bitwise_or(skeleton, temp, skeleton);
 
-        // Metoda 2: Kontury przez erozję
-        Mat contoursByErosion = new Mat();
-        Core.subtract(binaryImg, erodedImg, contoursByErosion);
-        Imgcodecs.imwrite("contours_by_erosion.png", contoursByErosion);
-        System.out.println("Zapisano: contours_by_erosion.png");
-
-        // Metoda 3: Kontury przez różnicę między dylatacją a erozją
-        Mat contoursByDifference = new Mat();
-        Core.subtract(dilatedImg, erodedImg, contoursByDifference);
-        Imgcodecs.imwrite("contours_by_difference.png", contoursByDifference);
-        System.out.println("Zapisano: contours_by_difference.png");
+            // Ustawienie erodowanego obrazu jako nowego obrazu wejściowego
+            eroded.copyTo(binaryImg);
+            // Sprawdzenie, czy obraz został całkowicie erodowany
+            done = Core.countNonZero(binaryImg) == 0;
+        } while (!done);
+        // Zapisanie obrazu szkieletu do pliku
+        String outputFilename = "skeleton.png";
+        Imgcodecs.imwrite(outputFilename, skeleton);
+        System.out.println("Zapisano: " + outputFilename);
     }
 }
