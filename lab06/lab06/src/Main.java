a98860297387656cd5e9b82bef0588be0700daa0
Pawelgabrieljonca <paweljonca30@gmail.com>
2024-11-23 12:53:05 +0100

    news

---
+++ lab06/lab06/src/Main.java
@@ -0,0 +1,11 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        // Ścieżka do obrazu wejściowego
+//        String inputPath = "geralt.jpg";
+//        // Ścieżka do obrazu wyjściowego
+//        String outputPath = "LaplacianEdge.jpg";
+//
+//        // Wywołanie metody do detekcji krawędzi
+//        EdgeDetection.detectLaplacianEdges(inputPath, outputPath);
+//    }
+//}
